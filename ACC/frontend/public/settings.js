ca1fb0087cd54ae1961d101dd715b28b2088bff4
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2016-02-27 20:11:31 +0100

    requesting Acs from MCS and showing them on screen
    request piped through node on ACCS to fix CORS headers issues with MCS

---
+++ ACC/frontend/public/settings.js
@@ -0,0 +1,6 @@
+define([''],
+    function () {
+        return {
+            baseUrl: 'https://frontend-metcsgse00739.apaas.us2.oraclecloud.com'
+        };
+    });
--- ACC/frontend/public/views/acts.js
+++ ACC/frontend/public/views/acts.js
@@ -1,7 +1,27 @@
-define(['knockout', 'ojs/ojcore'],
-    function (ko, oj) {
+define(['knockout', 'ojs/ojcore', 'settings', 'ojs/ojmodel', 'ojs/ojknockout-model'],
+    function (ko, oj, settings) {
         return ActsViewModel;
+
         function ActsViewModel() {
-            console.log('CREATING ACTS VIEWMODEL');
+            // PUBLIC API
+            this.acts = ko.observableArray();
+            this.handleActivated = handleActivated;
+
+            // ============================
+            // IMPLEMENTATION DETAILS BELOW
+            var self = this;
+
+            function handleActivated() {
+                var acts = new ActsCollection();
+                acts.fetch({
+                    success: function (coll, response, options) {
+                        self.acts(oj.KnockoutUtils.map(coll));
+                    }
+                });
+            }
+            var ActsCollection = oj.Collection.extend({
+                url: settings.baseUrl + '/mobile/custom/artistapi/acts'
+            });
         }
+
     });
