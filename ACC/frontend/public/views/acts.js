f83ab2ee0b7c8a10e0d4aa503b5e4df958011278
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2016-02-28 14:23:53 +0100

    allow sorting of acts
    also show alternative text while loading initial set of data

--- ACC/frontend/public/views/acts.js
+++ ACC/frontend/public/views/acts.js
@@ -1,22 +1,34 @@
-define(['knockout', 'ojs/ojcore', 'settings', 'ojs/ojmodel', 'ojs/ojknockout-model'],
+define(['knockout', 'ojs/ojcore', 'settings', 'ojs/ojmodel', 'ojs/ojknockout-model', 'ojs/ojbutton'],
     function (ko, oj, settings) {
         return ActsViewModel;
 
         function ActsViewModel() {
             // PUBLIC API
+            this.fetched = ko.observable(false);
             this.acts = ko.observableArray();
-            this.handleActivated = handleActivated;
+            this.sort = ko.observable('votes');
+            this.sortedActs = ko.computed(sortedActs, this);
+            this.handleActivated = handleActivated.bind(this);
 
             // ============================
             // IMPLEMENTATION DETAILS BELOW
-            var self = this;
-
             function handleActivated() {
+                var self = this;
                 var acts = new ActsCollection();
                 acts.fetch({
                     success: function (coll, response, options) {
                         self.acts(oj.KnockoutUtils.map(coll));
+                        self.fetched(true);
                     }
+                });
+            }
+            function sortedActs() {
+                var sortedActs = this.acts();
+                var sort = this.sort();
+                if (!sortedActs || !sort) { return sortedActs }
+                return sortedActs.sort(function (actA, actB) {
+                    if (sort === 'votes') { return actB.numberOfVotes() - actA.numberOfVotes(); }
+                    if (sort === 'date') { return actA.registrationDate() > actB.registrationDate() ? -1 : actA.registrationDate() < actB.registrationDate() ? 1 : 0 }
                 });
             }
             var ActsCollection = oj.Collection.extend({
