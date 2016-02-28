bf7bb4d5a3f990f8ffad3266dfae0967fb4031bb
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2016-02-28 17:23:31 +0100

    initial setup for a popup of act details

--- ACC/frontend/public/views/act.js
+++ ACC/frontend/public/views/act.js
@@ -1,10 +1,8 @@
-define(['knockout'],
-    function (ko) {
+define(['knockout', 'jquery', 'ojs/ojbutton', 'ojs/ojpopup'],
+    function (ko, $) {
         return ActViewModel;
 
         function ActViewModel(act) {
             this.act = act;
-            this.fetched = ko.observable(false);
         }
-
     });
