791301b72b8ed2ce02b08d789c454a5b2084cc02
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2016-02-28 16:30:11 +0100

    add chart to page with list of acts

---
+++ ACC/frontend/public/views/chart.js
@@ -0,0 +1,36 @@
+define(['knockout', 'ojs/ojchart', 'ojs/ojtoolbar', 'ojs/ojbutton'],
+    function (ko) {
+        var converterFactory = oj.Validation.converterFactory('number');
+        var votesConverter = converterFactory.createConverter({ minimumFractionDigits: 0, maximumFractionDigits: 0 });
+        return ChartViewModel;
+
+        function ChartViewModel(acts) {
+            this.acts = acts;
+            this.type = ko.observable('bar');
+            this.stack = ko.observable('off');
+            this.orientation = ko.observable('horizontal');
+            this.series = ko.computed(series, this);
+            this.votesConverter = ko.observable(votesConverter);
+
+            this.typeOptions = [
+                { id: 'bar', label: 'bar', value: 'bar', icon: 'oj-icon demo-bar-unstack' },
+                { id: 'pie', label: 'pie', value: 'pie', icon: 'oj-icon demo-bar-stack' }
+            ];
+            this.stackOptions = [
+                { id: 'unstacked', label: 'unstacked', value: 'off', icon: 'oj-icon demo-bar-unstack' },
+                { id: 'stacked', label: 'stacked', value: 'on', icon: 'oj-icon demo-bar-stack' }
+            ];
+            this.orientationOptions = [
+                { id: 'vertical', label: 'vertical', value: 'vertical', icon: 'oj-icon demo-bar-vert' },
+                { id: 'horizontal', label: 'horizontal', value: 'horizontal', icon: 'oj-icon demo-bar-horiz' }
+            ];
+        }
+        function series() {
+            if (this.type() === 'pie') {
+                return this.acts.map(function (act) { return { name: act.name(), items: [{ value: act.numberOfVotes(), label: act.numberOfVotes() }] } });
+            } else {
+                return this.acts.map(function (act) { return { name: act.name(), items: [act.numberOfVotes()] } });
+            }
+        }
+
+    });
