03f3e5352ff72a6213cfea311f19d321ffe7a2a8
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2016-03-12 13:13:07 +0100

    some addition logging for nodejs server

--- ACC/frontend/index.js
+++ ACC/frontend/index.js
@@ -8,6 +8,7 @@ app.use('/mobile/*', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     var url = process.env.MCS_URL + req.originalUrl;
+    console.log(new Date().toISOString() + ' forwaring request ' + req.originalUrl + ' to ' + url);
     req.pipe(request({
         url: url,
         headers: { 'oracle-mobile-backend-id': process.env.MCS_BACKEND_ID },
@@ -19,4 +20,5 @@ app.use('/mobile/*', function (req, res) {
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
     console.log('Example app listening on port ' + PORT + '!');
+    console.log('MCS backend id is ' + process.env.MCS_BACKEND_ID);
 });
