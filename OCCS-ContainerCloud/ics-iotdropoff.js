a1e1f8a7fc2ba3293c0882429a5c889416fccf4a
lucasjellema <lucasjellema@gmail.com>
2017-06-14 06:59:24 +0200

    Added ics-iotdropoff.js

---
+++ OCCS-ContainerCloud/ics-iotdropoff.js
@@ -0,0 +1,64 @@
+var https = require('https'),
+    http = require('http'),
+    fs = require('fs'),
+    //httpProxy = require('http-proxy'),
+    url = require('url'),
+    request = require('request');
+//    dateFormat = require('dateformat');
+
+var logger = require("./logger.js");
+var settings = require("./proxy-settings.js");
+
+var moduleName = "occs.icsProxy";
+
+var ICS_ENDPOINT = "https://ics4emeapartner-partnercloud17.integration.us2.oraclecloud.com/integration/flowapi/rest";
+var RESOURCE_IOTCS_DROPOFF = "ACEDEMO_IOTCSDROPO_INTEGRATIO/v01/act";
+
+var ICS_IOTCS_DROPOFF_ENDPOINT = ICS_ENDPOINT + '/' + RESOURCE_IOTCS_DROPOFF;
+
+var icsDropoffProxy = module.exports;
+
+icsDropoffProxy.reportArtistProposal = function (artist,countOfArtist ){
+   logger.log("Call ICS to report new artist proposal for  "+artist, moduleName, logger.DEBUG);
+
+   var options = {
+        method: 'POST',
+        url: ICS_IOTCS_DROPOFF_ENDPOINT
+        ,
+        headers:
+        {
+            'cache-control': 'no-cache',
+            authorization: 'Basic c3Zlbi5iZXJuaGFyZHRAb3BpdHotY29uc3VsdGluZy5jb206JHYzTjMzODFf',
+            'content-type': 'application/json'
+        },
+        body:
+        {
+            "id": 1,
+            "source": "ics-iotdropoff",
+            "sentTime": null,
+            "eventTime": null,
+            "payload": {
+                "data": {
+                       "count_of_data_artistname_15": countOfArtist
+                    ,  "max_of_data_artistname_12": artist
+                }
+            }
+        },
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log(moduleName + "- Error in processing IoT Dropoff message " + JSON.stringify(error));
+            throw new Error(error);
+        }
+        console.log(moduleName + " Forwarded IoT Dropoff Message to ICS: status= " + response.statusCode);
+
+    });
+
+
+
+
+}
+
+console.log(moduleName + " running against ICS Endpoint " + ICS_ENDPOINT);
